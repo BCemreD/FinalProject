68b45ed3fc20162c7edda896e2cf470f7ed4ea4f
BCemreD <72667928+BCemreD@users.noreply.github.com>
2021-03-07 18:57:39 +0300

    Add project files.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+       IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            var result = productService.GetAll();
+            return result.Data;
+        }
+    }
+}
